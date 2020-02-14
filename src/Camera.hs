ae1b87312c543b47dbdb5f0fbec6f35f910143fa
clockvoid <hidenari.t1997nov@gmail.com>
2020-02-15 04:02:29 +0900

    add: camera

---
+++ src/Camera.hs
@@ -0,0 +1,22 @@
+module Camera where
+
+import Vec
+import Ray
+
+data Camera = Camera Vec3 (Vec3, Vec3, Vec3)
+
+camera :: Vec3 -> Vec3 -> Vec3 -> Vec3 -> Camera
+camera m_origin m_u m_v m_w = Camera m_origin (m_u, m_v, m_w)
+
+cameraFromLookAt :: Vec3 -> Vec3 -> Vec3 -> Double -> Double -> Camera
+cameraFromLookAt lookfrom lookat vup vfov aspect = camera m_origin (2 * halfW |* u) (2 * halfH |* v) (m_origin - halfW |* u - halfH |* v - w)
+  where
+    halfH = tan vfov / 2.0
+    halfW = aspect * halfH
+    m_origin = vec3 0 0 0
+    w = normalize $ lookfrom - lookat
+    u = normalize $ cross vup w
+    v = cross w u
+
+getRay :: Double -> Double -> Camera -> Ray
+getRay u v (Camera m_origin (m_u, m_v, m_w)) = Ray m_origin (m_w + (m_u *| u) + (m_v *| v) - m_origin)
