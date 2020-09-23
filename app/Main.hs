eae8382b95b0fb923c4efee86db46be8451dddff
clockvoid <hidenari.t1997nov@gmail.com>
2020-09-23 17:32:35 +0900

    add: using vector

--- app/Main.hs
+++ app/Main.hs
@@ -2,33 +2,45 @@ module Main where
 
 import Text.Printf
 import Control.Monad
+import Vec
 
-height :: Float
+height :: Int
 height = 256
 
-width :: Float
+width :: Int
 width = 256
 
-eye = (0, 0, 5)
-sphere = (0, 0, 0)
+eye :: Vec3
+eye = vec3 0 0 5
+
+sphere :: Vec3
+sphere = vec3 0 0 0
+
+radius :: Double
 radius = 1
 
-intersectRaySphere :: (Float, Float, Float) -> (Float, Float, Float) -> (Float, Float, Float) -> Float -> Bool
-intersectRaySphere (ox, oy, oz) (dx, dy, dz) (cx, cy, cz) r = 0 <= (b * b - 4 * a * c)
+calcPrimaryRay :: (Int, Int) -> Vec3
+calcPrimaryRay (x, y) = normalize $ vec3 dx dy dz
   where
-   a = dx * dx + dy * dy + dz * dz
-   b = 2 * (dx * (ox - cx) + dy * (oy - cy) + dz * (oz - cz))
-   c = (ox - cx) ** 2 + (oy - cy) ** 2 + (oz - cz) ** 2 - r ** 2
+    dx = fromIntegral x + 0.5 - fromIntegral width / 2
+    dy = -(fromIntegral y + 0.5 - fromIntegral height / 2)
+    dz = -(fromIntegral height)
 
-calcPixelColor :: (Float, Float) -> (Int, Int, Int)
--- calcPixelColor(x, y) = (round (abs (x + 0.5 - width / 2)), round (abs (y + 0.5 - height / 2)), 0)
-calcPixelColor (x, y) = if intersectRaySphere eye ray sphere radius
+intersectRaySphere :: Vec3 -> Vec3 -> Vec3 -> Double -> Bool
+intersectRaySphere rayOrigin rayDir sphereCenter sphereRadius = 0 <= (b * b - c)
+  where
+   v = rayOrigin - sphereCenter
+   b = rayDir `dot` v
+   c = v `dot` v - sphereRadius ** 2
+
+calcPixelColor :: (Int, Int) -> (Int, Int, Int)
+calcPixelColor dir = if intersectRaySphere eye ray sphere radius
                         then
                           (255, 255, 255)
                         else
                           (0, 0, 0)
                           where
-                            ray = (x + 0.5 - width / 2, -(y + 0.5 - height / 2), -height)
+                            ray = calcPrimaryRay dir
 
 draw :: [(Int, Int, Int)]
 draw = map calcPixelColor [(x, y) | y <- [0..width - 1], x <- [0..height - 1]]
