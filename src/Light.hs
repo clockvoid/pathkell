5b2e63e6be2514a74d088f7f5a527dd9f42a5bd0
clockvoid <hidenari.t1997nov@gmail.com>
2020-09-30 21:21:45 +0900

    apply: struct to light.hs

--- src/Light.hs
+++ src/Light.hs
@@ -3,11 +3,8 @@ module Light where
 import Base.Vec
 import Base.Spectrum
 
-data Light = Light Vec3 Spectrum deriving (Eq, Show)
-
-pos :: Light -> Vec3
-pos (Light position _) = position
-
-power :: Light -> Spectrum
-power (Light _ pow) = pow
+data Light = Light
+  { pos :: Vec3
+  , power :: Spectrum
+  } deriving (Eq, Show)
 
